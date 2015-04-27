7933b2e67208c2e9292c55a40babe22ca72eafed
kubkaray <Kubilay.Karayilan@inmeta.no>
2015-04-27 13:48:11 +0200

    Initial commit

---
+++ src/main/java/ViewConf.java
@@ -0,0 +1,12 @@
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by kubkaray on 27.04.2015.
+ */
+public class ViewConf extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/index.html");
+    }
+}
