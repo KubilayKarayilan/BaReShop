4c4743be2a944a0fb9f3f5336c9eea8d2b12c90f
kubkaray <Kubilay.Karayilan@inmeta.no>
2015-04-27 14:15:40 +0200

    Welcome Screen

--- src/main/java/MainCtrl.java
+++ src/main/java/MainCtrl.java
@@ -2,7 +2,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by kubkaray on 22.04.2015.
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @EnableAutoConfiguration
 public class MainCtrl {
     @RequestMapping("/")
-    @ResponseBody
+
     String mainCtrl() {
         return "forward:/index.html";
     }
